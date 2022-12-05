1b75c0a73de798b5058cb6c2bbf67ef3d03a94fa
Jae <39260243+Spoicy@users.noreply.github.com>
2022-12-05 14:18:18 +0100

    Part 2 of Day 5 solution (ugly)

---
+++ src/main/java/ch/spoicy/day05/Main1.java
@@ -0,0 +1,55 @@
+package ch.spoicy.day05;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main1 {
+
+    private static final Logger LOG =
+            LogManager.getLogger(Main1.class);
+
+    public static void main(String[] args) {
+        BufferedReader reader;
+        List<Deque<Character>> dequeList = new ArrayList<>();
+        try {
+            reader = new BufferedReader(new FileReader("C:\\aoc2022inputs\\day05.txt"));
+            String line = reader.readLine();
+            // Read stacks
+            for (int i = 1; i < line.length(); i+=4) {
+                dequeList.add(new ArrayDeque<>());
+            }
+            while (line.charAt(1) != '1') {
+                for (int i = 0; i < dequeList.size(); i++) {
+                    if (line.charAt(i*4+1) != ' ') {
+                        dequeList.get(i).add(line.charAt(i*4+1));
+                    }
+                }
+                line = reader.readLine();
+            }
+            reader.readLine();
+            line = reader.readLine();
+            while (line != null) {
+                String[] lineSplit = line.split("move | from | to ");
+                for (int i = 0; i < Integer.parseInt(lineSplit[1]); i++) {
+                    char c = dequeList.get(Integer.parseInt(lineSplit[2]) - 1).pop();
+                    dequeList.get(Integer.parseInt(lineSplit[3]) - 1).push(c);
+                }
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String output = "";
+        for (int i = 0; i < dequeList.size(); i++) {
+            output += dequeList.get(i).getFirst();
+        }
+        System.out.println(output);
+    }
+}
--- src/main/java/ch/spoicy/day05/Main2.java
+++ src/main/java/ch/spoicy/day05/Main2.java
@@ -6,38 +6,46 @@ import org.apache.logging.log4j.Logger;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
-public class Main {
+public class Main2 {
 
     private static final Logger LOG =
-            LogManager.getLogger(Main.class);
+            LogManager.getLogger(Main2.class);
 
     public static void main(String[] args) {
         BufferedReader reader;
-        List<Deque<Character>> list = new ArrayList<>();
+        List<Deque<Character>> dequeList = new ArrayList<>();
         try {
             reader = new BufferedReader(new FileReader("C:\\aoc2022inputs\\day05.txt"));
             String line = reader.readLine();
             // Read stacks
             for (int i = 1; i < line.length(); i+=4) {
-                list.add(new ArrayDeque<>());
+                dequeList.add(new ArrayDeque<>());
             }
             while (line.charAt(1) != '1') {
-                for (int i = 0; i < list.size(); i++) {
+                for (int i = 0; i < dequeList.size(); i++) {
                     if (line.charAt(i*4+1) != ' ') {
-                        list.get(i).add(line.charAt(i*4+1));
+                        dequeList.get(i).add(line.charAt(i*4+1));
                     }
                 }
                 line = reader.readLine();
             }
+            reader.readLine();
             line = reader.readLine();
-            line = reader.readLine();
+            Deque<Character> tempList = new ArrayDeque<>();
             while (line != null) {
-                String[] test = line.split("move | from | to ");
-                for (int i = 0; i < Integer.parseInt(test[1]); i++) {
-                    char c = list.get(Integer.parseInt(test[2]) - 1).pop();
-                    list.get(Integer.parseInt(test[3]) - 1).push(c);
+                tempList.clear();
+                String[] lineSplit = line.split("move | from | to ");
+                for (int i = 0; i < Integer.parseInt(lineSplit[1]); i++) {
+                    char c = dequeList.get(Integer.parseInt(lineSplit[2]) - 1).pop();
+                    tempList.push(c);
+                }
+                for (int i = 0; i < Integer.parseInt(lineSplit[1]); i++) {
+                    dequeList.get(Integer.parseInt(lineSplit[3]) - 1).push(tempList.pop());
                 }
                 // read next line
                 line = reader.readLine();
@@ -47,8 +55,8 @@ public class Main {
             e.printStackTrace();
         }
         String output = "";
-        for (int i = 0; i < list.size(); i++) {
-            output += list.get(i).getFirst();
+        for (int i = 0; i < dequeList.size(); i++) {
+            output += dequeList.get(i).getFirst();
         }
         System.out.println(output);
     }
