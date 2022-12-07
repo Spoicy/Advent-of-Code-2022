b67235390f1cacc4de7514905d09a2d41c5676df
Jae <39260243+Spoicy@users.noreply.github.com>
2022-12-07 10:41:36 +0100

    Day 7 current state

---
+++ src/main/java/ch/spoicy/day07/Main.java
@@ -0,0 +1,36 @@
+package ch.spoicy.day07;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    private static final Logger LOG =
+            LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader("E:\\aoc2022inputs\\input.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                if (line.contains("$ cd")) {
+
+                } else if (line.contains("$ ls")) {
+
+                }
+                System.out.println(line);
+                // read next line
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
